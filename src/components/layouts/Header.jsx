43b336c654ec6e37e5cff4310ab6e482ecd15a7c
Mansoor Akhter <mansoor21st@gmail.com>
2024-11-05 22:42:27 +0500

    header, hero done with responsive

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,31 +1,65 @@
+import { useState } from "react";
 import "../../styles/HeaderStyle.css";
 import { Link } from "react-router-dom";
-import { NavDropdown, Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../../assets/logo/logo.png";
 
 const Header = () => {
+  const [nav, setNav] = useState(false);
+
+  // Scroll navbar
+  const changeValueOnScroll = () => {
+    const scrollValue = document?.documentElement?.scrollTop;
+    scrollValue > 150 ? setNav(true) : setNav(false);
+  };
+
+  window.addEventListener("scroll", changeValueOnScroll);
+
   return (
-    <Navbar collapseOnSelect expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <Link to="/">
-            <img src={logo} alt="Brand logo" />
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="m-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/cart">Our Menu</Nav.Link>
-            <Nav.Link as={Link} to="/cart">Shop</Nav.Link>
-            <Nav.Link as={Link} to="/cart">Blog</Nav.Link>
-            <Nav.Link as={Link} to="/cart">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/cart"><i className="bi bi-bag fs-5s"></i> <em className="roundpoint">2</em></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={`${nav === true ? "sticky" : "hidden"}`}
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <Link to="/" className="logo">
+              <img src={logo} alt="Brand logo" />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="m-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Our Menu
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Shop
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Contact
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                <div className="cart">
+                  <i className="bi bi-bag fs-5s"></i>
+                  <em className="roundpoint">2</em>
+                </div>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Layout from '../../components/layouts/Layout'
+import React from "react";
+import Layout from "../../components/layouts/Layout";
+import Section1 from "./Section1";
+import "../../styles/HomeStyle.css";
 
 const Home = () => {
   return (
-  <Layout>HOME</Layout>
-  )
-}
+    <Layout>
+      <Section1 />
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
